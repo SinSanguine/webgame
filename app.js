a75eb4215eafee2f50c29079804d6334a4f5181c
Sanguine <sin@clouditem.co>
2024-09-22 20:40:32 +0000

    mqtt connected

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const aedes = require('aedes')({
+    preConnect: (client, packet, negate) => console.log('Connected: ', packet.clientId) || negate(null, true),
+    // published: (packet, client, done) => console.log(packet.topic) || done(),
+});
+const http = require('http');
+const ws = require('websocket-stream');
 
 const app = express();
 
 app.use(express.static('./static'));
 
-app.listen(process.env.PORT, () => console.log('Server listening on ' + process.env.PORT));
+app.listen(process.env.PORT, () => console.log('Server listening on ', process.env.PORT));
+
+const mqtt_port = process.env.MQTT_PORT || 8333;
+const httpServer = http.createServer();
+ws.createServer({ server: httpServer }, aedes.handle);
+httpServer.listen(mqtt_port, () => {
+    console.log('websocket server listening on port ', mqtt_port);
+});
+
--- static/webgl.js
+++ static/webgl.js
@@ -24,20 +24,46 @@ let aVertexPosition;
 
 // Animation timing
 
+const size = 0.1;
+const degreesPerSecond = 180.0;
+const client_id = (Math.random() + 1).toString(36).substring(7);
+const topic = `players/${client_id}`;
+const sync_time = 5;
 let previousTime = 0.0;
-let degreesPerSecond = 90.0;
+/**@type {{[client_id:string]: [number,number]}} */
+let players = {
 
+};
 window.addEventListener("load", startup, false);
 
-function startup() {
-    const glCanvas = document.getElementById("glcanvas");
-    if (!glCanvas || !(glCanvas instanceof HTMLCanvasElement))
-        throw Error('canvas wrong')
-    const gl = glCanvas.getContext("webgl");
-    if (!gl)
-        throw Error('no webgl context');
+async function startup() {
+    try {
+        /** @type {{default: import('mqtt')}} */
+        const { default: mqtt } = await import('https://unpkg.com/mqtt@5.3.0/dist/mqtt.esm.js');
+        const mqtt_client = await mqtt.connectAsync('ws://localhost:8333/');
+        mqtt_client.subscribe('players/+', err => {
 
-    const shader_config =
+            if (err)
+                console.log(err);
+
+        });
+        mqtt_client.on('message', (incoming_topic, message_buffer) => {
+
+            const message = message_buffer.toString('utf8');
+            console.log('message', incoming_topic, message);
+            if (incoming_topic !== topic)
+                players[topic] = JSON.parse(message);
+
+
+        });
+        const glCanvas = document.getElementById("glcanvas");
+        if (!glCanvas || !(glCanvas instanceof HTMLCanvasElement))
+            throw Error('canvas wrong')
+        const gl = glCanvas.getContext("webgl");
+        if (!gl)
+            throw Error('no webgl context');
+
+        const shader_config =
 /** 
   * @satisfies {{[shader_name:string]: {
   * type: typeof gl.VERTEX_SHADER | typeof gl.FRAGMENT_SHADER;
@@ -46,196 +72,205 @@ function startup() {
   * code: string;
   * }}} 
   */ ({
-            vertex_shader: {
-                type: gl.VERTEX_SHADER,
-                variables: {
-                    aVertexPosition: {
-                        type: 'vec2',
-                        scope: 'attribute',
+                vertex_shader: {
+                    type: gl.VERTEX_SHADER,
+                    variables: {
+                        aVertexPosition: {
+                            type: 'vec2',
+                            scope: 'attribute',
+                        },
+                        uScalingFactor: {
+                            type: 'vec2',
+                            scope: 'uniform',
+                        },
+                        uRotationVector: {
+                            type: 'vec2',
+                            scope: 'uniform',
+                        },
+                        u_position_adjust: {
+                            type: 'vec2',
+                            scope: 'uniform',
+                        }
                     },
-                    uScalingFactor: {
-                        type: 'vec2',
-                        scope: 'uniform',
-                    },
-                    uRotationVector: {
-                        type: 'vec2',
-                        scope: 'uniform',
-                    },
-                    u_position_adjust: {
-                        type: 'vec2',
-                        scope: 'uniform',
-                    }
-                },
-                code: `
+                    code: `
 void main() {
   vec2 rotatedPosition = vec2(
     aVertexPosition.x * uRotationVector.y +
-          aVertexPosition.y * uRotationVector.x +
-          u_position_adjust.x,
+          aVertexPosition.y * uRotationVector.x,
     aVertexPosition.y * uRotationVector.y -
-          aVertexPosition.x * uRotationVector.x +
-          u_position_adjust.y
+          aVertexPosition.x * uRotationVector.x
   );
-  gl_Position = vec4(rotatedPosition * uScalingFactor, 0.0, 1.0);
+  gl_Position = vec4(rotatedPosition * uScalingFactor + u_position_adjust, 0.0, 1.0);
 }
 `
-            },
-            fragment_shader: {
-                type: gl.FRAGMENT_SHADER,
-                variables: {
-                    uGlobalColor: {
-                        type: 'vec4',
-                        scope: 'uniform',
-                        default_value: [0.1, 0.7, 0.2, 1.0],
-                    }
                 },
-                prefix: `
+                fragment_shader: {
+                    type: gl.FRAGMENT_SHADER,
+                    variables: {
+                        uGlobalColor: {
+                            type: 'vec4',
+                            scope: 'uniform',
+                            default_value: [0.1, 0.7, 0.2, 1.0],
+                        }
+                    },
+                    prefix: `
 precision highp float;
 `,
-                code: `
+                    code: `
 void main() {
         gl_FragColor = uGlobalColor;
 }
 `},
-        });
+            });
 
-    const shaders = map_object(shader_config, ({ code, type, variables, prefix = '' }) => {
+        const shaders = map_object(shader_config, ({ code, type, variables, prefix = '' }) => {
 
-        const shader = gl.createShader(type);
-        if (!shader)
-            throw Error('Error creating Shader')
-        gl.shaderSource(shader, `
+            const shader = gl.createShader(type);
+            if (!shader)
+                throw Error('Error creating Shader')
+            gl.shaderSource(shader, `
 ${prefix}
 
 ${Object.entries(variables).map(([variable_name, { type, scope }]) => `${scope} ${type} ${variable_name};`).join('\n')}
 
 ${code}
 `);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            console.log(
-                `Error compiling ${type === gl.VERTEX_SHADER ? "vertex" : "fragment"
-                } shader:`,
-            );
-            console.log(gl.getShaderInfoLog(shader));
-        }
-        return shader;
-    });
-
-
-    const shaderProgram = gl.createProgram();
-    if (!shaderProgram)
-        throw Error('error creating shader program');
-    Object.values(shaders).forEach((shader) => {
-        gl.attachShader(shaderProgram, shader);
-    });
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.log("Error linking shader program:");
-        console.log(gl.getProgramInfoLog(shaderProgram));
-    }
-    gl.useProgram(shaderProgram);
-
-
-    /**@type {{[shader_name in keyof typeof shader_config]: {[variable in keyof typeof shader_config[shader_name]['variables']]: variable_type_to_type[typeof shader_config[shader_name]['variables'][variable]['type']]}}} */
-    //@ts-expect-error
-    const variables = map_object(shader_config, ({ variables: vars }) => {
-
-        const result = {};
-        Object.defineProperties(result, map_object(vars, /** @return {PropertyDescriptor} */({ type, scope, default_value }, variable_name) => {
-            const location = gl[scope_to_location_function[scope]](shaderProgram, variable_name);
-            /**@type {unknown} */
-            let temp = default_value;
-            if (default_value) {
-                gl[scope_to_type_to_set_function[scope][type]](location, default_value);
+            gl.compileShader(shader);
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+                console.log(
+                    `Error compiling ${type === gl.VERTEX_SHADER ? "vertex" : "fragment"
+                    } shader:`,
+                );
+                console.log(gl.getShaderInfoLog(shader));
             }
-            return ({
-                enumerable: true,
-                get: () => temp,
-                set: (val) => {
-                    gl[scope_to_type_to_set_function[scope][type]](location, val);
-                    temp = val;
-                },
-            })
-        }))
-        return result;
-    });
-
-    /**@type {[number,number]} */
-    let currentRotation = [0, 1];
-
-    vertexArray = new Float32Array([
-        -0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5,
-    ]);
-
-    vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
-
-    vertexNumComponents = 2;
-    vertexCount = vertexArray.length / vertexNumComponents;
-
-    let currentAngle = 0.0;
-
-    let x = 0;
-    let y = 0;
-
-    glCanvas.addEventListener('mousemove', (event) => {
-        console.log(event);
-        const { width: css_width, height: css_height } = getComputedStyle(glCanvas);
-        const { width, height } = { width: Number.parseInt(css_width.slice(0, -1 * 'px'.length)), height: Number.parseInt(css_height.slice(0, -1 * 'px'.length)) };
-        x = event.clientX / width * 2 - 1;
-        y = - event.clientY / height * 2 + 1;
-        console.log(event.clientX, width, x);
-        console.log(event.clientY, height, y);
-
-    })
-
-    const animateScene = () => {
-        resizeCanvasToDisplaySize(glCanvas);
-        const { width: css_width, height: css_height } = getComputedStyle(glCanvas);
-        const { width, height } = { width: Number.parseInt(css_width.slice(0, -1 * 'px'.length)), height: Number.parseInt(css_height.slice(0, -1 * 'px'.length)) }
-        gl.viewport(0, 0, width, height);
-        gl.clearColor(0, 0, 0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        const radians = (currentAngle * Math.PI) / 180.0;
-        currentRotation[0] = Math.sin(radians);
-        currentRotation[1] = Math.cos(radians);
-
-
-        variables.vertex_shader.uScalingFactor = [height > width ? 1.0 : height / width, width > height ? 1.0 : width / height];
-        variables.vertex_shader.uRotationVector = currentRotation;
-        variables.vertex_shader.u_position_adjust = [x, y];
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-
-        aVertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-
-        gl.enableVertexAttribArray(aVertexPosition);
-        gl.vertexAttribPointer(
-            aVertexPosition,
-            vertexNumComponents,
-            gl.FLOAT,
-            false,
-            0,
-            0,
-        );
-
-        gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
-
-        requestAnimationFrame((currentTime) => {
-            const deltaAngle =
-                ((currentTime - previousTime) / 1000.0) * degreesPerSecond;
-
-            currentAngle = (currentAngle + deltaAngle) % 360;
-
-            previousTime = currentTime;
-            animateScene();
+            return shader;
         });
-    };
 
-    animateScene();
+
+        const shaderProgram = gl.createProgram();
+        if (!shaderProgram)
+            throw Error('error creating shader program');
+        Object.values(shaders).forEach((shader) => {
+            gl.attachShader(shaderProgram, shader);
+        });
+        gl.linkProgram(shaderProgram);
+        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+            console.log("Error linking shader program:");
+            console.log(gl.getProgramInfoLog(shaderProgram));
+        }
+        gl.useProgram(shaderProgram);
+
+
+        /**@type {{[shader_name in keyof typeof shader_config]: {[variable in keyof typeof shader_config[shader_name]['variables']]: variable_type_to_type[typeof shader_config[shader_name]['variables'][variable]['type']]}}} */
+        //@ts-expect-error
+        const variables = map_object(shader_config, ({ variables: vars }) => {
+
+            const result = {};
+            Object.defineProperties(result, map_object(vars, /** @return {PropertyDescriptor} */({ type, scope, default_value }, variable_name) => {
+                const location = gl[scope_to_location_function[scope]](shaderProgram, variable_name);
+                /**@type {unknown} */
+                let temp = default_value;
+                if (default_value) {
+                    gl[scope_to_type_to_set_function[scope][type]](location, default_value);
+                }
+                return ({
+                    enumerable: true,
+                    get: () => temp,
+                    set: (val) => {
+                        gl[scope_to_type_to_set_function[scope][type]](location, val);
+                        temp = val;
+                    },
+                })
+            }))
+            return result;
+        });
+
+        /**@type {[number,number]} */
+        let currentRotation = [0, 1];
+
+        vertexArray = new Float32Array([
+            -size, size, size, size, size, -size, -size, size, size, -size, -size, -size,
+        ]);
+
+        vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
+
+        vertexNumComponents = 2;
+        vertexCount = vertexArray.length / vertexNumComponents;
+
+        let currentAngle = 0.0;
+
+
+        let last_time = Date.now();
+
+        glCanvas.addEventListener('mousemove', (event) => {
+            const { width: css_width, height: css_height } = getComputedStyle(glCanvas);
+            const { width, height } = { width: Number.parseInt(css_width.slice(0, -1 * 'px'.length)), height: Number.parseInt(css_height.slice(0, -1 * 'px'.length)) };
+            players[client_id] = [
+                (event.clientX / width * 2 - 1),
+                (-event.clientY / height * 2 + 1)
+            ];
+            if ((last_time + sync_time) < Date.now()) {
+                last_time = Date.now();
+                mqtt_client.publishAsync(topic, JSON.stringify(players[client_id]), { qos: 1, retain: true })
+                    .then(() => console.log(topic))
+                    .catch(console.log);
+            }
+            // console.log(event.clientX, width, x);
+            // console.log(event.clientY, height, y);
+        })
+
+        const animateScene = () => {
+            resizeCanvasToDisplaySize(glCanvas);
+            const { width: css_width, height: css_height } = getComputedStyle(glCanvas);
+            const { width, height } = { width: Number.parseInt(css_width.slice(0, -1 * 'px'.length)), height: Number.parseInt(css_height.slice(0, -1 * 'px'.length)) }
+            gl.viewport(0, 0, width, height);
+            gl.clearColor(0, 0, 0, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            const radians = (currentAngle * Math.PI) / 180.0;
+            currentRotation[0] = Math.sin(radians);
+            currentRotation[1] = Math.cos(radians);
+
+
+            variables.vertex_shader.uScalingFactor = [height > width ? 1.0 : height / width, width > height ? 1.0 : width / height];
+            variables.vertex_shader.uRotationVector = currentRotation;
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+            aVertexPosition = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+
+            gl.enableVertexAttribArray(aVertexPosition);
+            gl.vertexAttribPointer(
+                aVertexPosition,
+                vertexNumComponents,
+                gl.FLOAT,
+                false,
+                0,
+                0,
+            );
+
+            Object.values(players).forEach(position => {
+                variables.vertex_shader.u_position_adjust = position;
+                gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
+            });
+
+            requestAnimationFrame((currentTime) => {
+                const deltaAngle =
+                    ((currentTime - previousTime) / 1000.0) * degreesPerSecond;
+
+                currentAngle = (currentAngle + deltaAngle) % 360;
+
+                previousTime = currentTime;
+                animateScene();
+            });
+        };
+        animateScene();
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 
